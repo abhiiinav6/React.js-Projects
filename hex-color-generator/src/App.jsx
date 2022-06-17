668642422bcdf978bc25493170280d303bb05c65
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-06-17 14:08:52 -0700

    hh

--- hex-color-generator/src/App.jsx
+++ hex-color-generator/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import Layout from "./Layout"
+import { useState, useEffect } from 'react'
+import Layout from './Layout'
+import "./index.css"
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [colorArray, setColorArray] = useState(["#9c5c50", "#70ca3b", "#3e67f0"])
+  const randomHexColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+  function addingColor() {
+    let arr = []
+    for (let i = 0; i < 10; i++) {
+      let color = randomHexColor()
+      arr.push(color)
+    }
+    setColorArray([...arr])
+  }
+  useEffect(() => {
+    addingColor()
+    console.log(colorArray)
+  }, [])
   return (
     <div className="App">
       <h1>Random Hex Colors</h1>
-      <Layout />
+      <Layout colorArray={colorArray} />
     </div>
   )
 }
--- hex-color-generator/src/Layout.jsx
+++ hex-color-generator/src/Layout.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Box from './Box';
-function Layout() {
-    const [colorArray, setColorArray] = useState([])
-    const randomHexColor = () => {
-        return '#' + Math.floor(Math.random() * 16777215).toString(16);
-    }
-    for (let i = 0; i < 30; i++) {
-        setColorArray(arr => arr.push(randomHexColor()))
-    }
+function Layout(colorArray) {
+
     return (
         <div className='layout'>
+
             {
-                colorArray.map(color => {
-                    <Box colorCode={color} />
-                })
+                colorArray.length !== 0 ?
+                    colorArray.map((color) => {
+                        return <Box colorCode={color} />
+                    })
+                    : null
+
             }
         </div>
     )
