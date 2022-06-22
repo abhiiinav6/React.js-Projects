0f3d49ee437af70eb16414f08643cb148e88ff43
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-06-22 17:58:34 +0530

    Completed Navbar

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+  return (<>
+    <NavBar />
+    <Container className="bg-light mb-4 mx-0">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Container> </>
   );
 }
 
