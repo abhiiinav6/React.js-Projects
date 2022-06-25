8203f95560ff58db71d92a2da56db3f86a6bf937
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-06-25 18:02:33 +0530

    Added Images

--- shopping-cart/src/pages/Store.tsx
+++ shopping-cart/src/pages/Store.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import storeItems from "../data/item.json"
+import { Row, Col } from "react-bootstrap"
 
 function Store() {
   return (
-    <div>Store</div>
+    <>
+      <div>Store</div>
+      <Row>
+        {storeItems.map(item => {
+          return (
+            <Col>{JSON.stringify(item)}</Col>
+          )
+        })}
+      </Row>
+    </>
   )
 }
 
