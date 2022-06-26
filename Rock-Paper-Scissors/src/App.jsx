3f6b6f32b137f03fb5820f8ce531434a49abd10f
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-06-26 10:13:26 +0530

    Initialized Project

---
+++ Rock-Paper-Scissors/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return(
+    <h1 className='text-3xl text-blue-700'>Namaste World</h1>
+  )
+}
+
+export default App
