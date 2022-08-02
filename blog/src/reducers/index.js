4049cca3650531376c96352157dc407be06c2d24
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-08-03 05:08:55 +0530

    added postReducer and exported it into reducer main file

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import postReducer from "./postReducer";
+
 
 export default combineReducers({
-    hello: 10
+    posts: postReducer
 })
---
+++ blog/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const postReducer = (state = [], action) => {
+    switch (action.type) {
+        case "FETCH_POST":
+            return action.payload;
+        case "ADD_POST":
+            return [...state, action.payload]
+        default:
+            return state;
+    }
+}
+
+export default postReducer
