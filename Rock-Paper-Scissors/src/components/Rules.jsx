70b2ab181160598b4494954d2c0a555c39debde6
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-08-02 12:07:12 +0530

    completed rules design

--- Rock-Paper-Scissors/src/components/Rules.jsx
+++ Rock-Paper-Scissors/src/components/Rules.jsx
@@ -15,7 +15,7 @@ function Rules() {
 
 
           <div className=' flex flex-col'>
-            <h3 className='my-0'>Beats</h3>
+            <h3 className='my-0 text-2xl'>Beats</h3>
             <h1 className='text-[5rem] my-[-3rem]'>
               &#8592;
             </h1>
@@ -27,13 +27,18 @@ function Rules() {
           </div>
 
         </div>
-        <h3 className='oo'>&#8593;</h3>
-        <div className='h-28 w-28 bg-white flex items-center justify-center rounded-full border-[0.8rem] cursor-pointer border-red-600 mt-8 mx-auto'>
+        <h3 className='text-7xl rotate-[30deg] translate-x-16 m-0'>&#8593;</h3>
+        <h3 className='mx-4 p-0 text-2xl rotate-[-60deg] translate-x-20 -translate-y-8'>Beats</h3>
+        <h3 className='text-7xl rotate-[-30deg] -translate-x-16 -translate-y-24 m-0'>&#8595;</h3>
+        <h3 className='my-0 mx-8 p-0 text-2xl rotate-[60deg] -translate-x-20 -translate-y-32'>
+          Beats
+        </h3>
+
+
+        <div className='h-28 w-28 bg-white flex items-center justify-center rounded-full border-[0.8rem] cursor-pointer border-red-600 -mt-28 mx-auto'>
           <img src="/images/icon-rock.svg" alt="" />
         </div>
-
       </div>
-
     </div>
   )
 }
