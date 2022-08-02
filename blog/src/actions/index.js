a12de4efc59b0c893e09c5f9670728498eeec35c
Abhinav Kumar <abhinavkumar34002@gmail.com>
2022-08-03 05:27:30 +0530

    completed reducer function

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder"
 
+
+
 export const fetchPost = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts')
 
-    dispatch({ typr: "FETCH_POST", payload: response })
+    dispatch({ type: "FETCH_POST", payload: response.data })
 }
 
 
--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchPost } from "../actions";
+import { useSelector, useDispatch } from "react-redux";
 
-function PostList() {
+function PostList({}) {
+    const dispatch = useDispatch()
+    const posts = useSelector(state => state.posts)
+  useEffect(() => {
+    // fetchPost()
+    dispatch(fetchPost())
+  }, [])
+  console.log(posts);
+
   return (
     <div>
       <h1>PostList</h1>
+      {
+        posts.map(post=>(
+          <li>{post.title}</li>
+        ))
+      }
     </div>
   );
 }
+// const mapStateToProps = state => ({ posts: state.posts })
+// export default connect(mapStateToProps, {
+//   fetchPost,
+// })(PostList);
 
-export default connect(null, {
-  fetchPost,
-})(PostList);
+export default PostList
